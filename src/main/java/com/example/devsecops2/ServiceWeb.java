65108e2c378d1b147237911464f6410b9aed4ed5
Dylan Zakaharinivo <zakadylan2005@gmail.com>
2025-03-25 17:50:29 +0100

    dacia

--- src/main/java/com/example/devsecops2/ServiceWeb.java
+++ src/main/java/com/example/devsecops2/ServiceWeb.java
@@ -18,7 +18,7 @@ public class ServiceWeb {
         cars.add(car);
         car = new Car("CC33DD", "peugeot", 500);
         cars.add(car);
-        car = new Car("DD44EE", "Dacia", 1900);
+        car = new Car("DD44EE", "Dacia", 2500);
         cars.add(car);
     }
 
